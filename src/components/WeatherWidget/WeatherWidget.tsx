fce7ec44770b7622d770110b7fd2458cd612f8e1
cee <hungns126@gmail.com>
2021-04-12 09:53:51 +0700

    remove comment

--- src/components/WeatherWidget/WeatherWidget.tsx
+++ src/components/WeatherWidget/WeatherWidget.tsx
@@ -14,7 +14,6 @@ interface IWeatherWidget {
 
 export default function WeatherWidget(props: IWeatherWidget) {
   const { coordinates, widgetData, airQuality } = props;
-  // const [temperature, setTemperature] = useState({ type: 'c', temp: widgetData?.temperature && Math.round(widgetData?.temperature * 10) / 10 })
   const [temperature, setTemperature] = useState({ type: 'c', temp: widgetData?.temperature && Math.round(widgetData?.temperature) })
   const isC = temperature.type === 'c'
   const airQualityIndex = airQuality?.list?.[0]?.main?.aqi
@@ -23,8 +22,6 @@ export default function WeatherWidget(props: IWeatherWidget) {
     widgetData?.temperature && setTemperature(prev => ({
       ...prev,
       temp: prev.type === 'c'
-        // ? Math.round(widgetData?.temperature * 10) / 10
-        // : fcConverter(Math.round(widgetData?.temperature * 10) / 10, 'f')
         ? Math.round(widgetData?.temperature)
         : fcConverter(Math.round(widgetData?.temperature), 'f')
     }))
@@ -42,7 +39,6 @@ export default function WeatherWidget(props: IWeatherWidget) {
         <div className='temperature-wrapper'>
           <img src={`http://openweathermap.org/img/wn/${widgetData?.icon}@2x.png`} width="55" height="55" />
           <div className='temperature'>{Math.round(temperature?.temp)}°</div>
-          {/* <div className='temperature'>{Math.round((temperature?.temp + Number.EPSILON) * 100) / 100}°</div> */}
           <div className='degree-wrapper'>
             <span className={`degree ${!isC && 'degree-active'}`} onClick={onChangeDegree('f')}>F</span>
             <span> / </span>
